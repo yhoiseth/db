fa13a3bf9ca1e245220ed26d9f5a22b02676ba22
Yngve Høiseth <yngve@hoiseth.net>
2019-10-20 21:46:15 +0200

    Add homepage introduction

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
-from django.db import models
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.blocks import RichTextBlock
+from wagtail.core.fields import StreamField
 
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+    introduction = StreamField([("rich_text", RichTextBlock())], blank=True)
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("introduction")
+    ]
