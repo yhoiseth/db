987ccc379c194b45acbadc872775fca3ae3985b1
Yngve Høiseth <yngve@hoiseth.net>
2019-10-21 22:28:55 +0200

    Debug CSS

--- project/settings/production.py
+++ project/settings/production.py
@@ -3,4 +3,7 @@ from django_heroku import settings
 from .base import *  # noqa: F401, F403
 
 DEBUG = False
+COMPRESS_OFFLINE = True
+LIBSASS_OUTPUT_STYLE = "compressed"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 settings(locals())
