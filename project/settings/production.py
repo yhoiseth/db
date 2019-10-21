15094fabf2eea34072435cd478e0d7f6e784c1e0
Yngve Høiseth <yngve@hoiseth.net>
2019-10-21 22:44:35 +0200

    Disable debug

--- project/settings/production.py
+++ project/settings/production.py
@@ -2,7 +2,7 @@ from django_heroku import settings
 
 from .base import *  # noqa: F401, F403
 
-DEBUG = True
+DEBUG = False
 COMPRESS_OFFLINE = True
 LIBSASS_OUTPUT_STYLE = "compressed"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
